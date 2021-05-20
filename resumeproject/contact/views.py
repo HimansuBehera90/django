5adff4d21320e6f8896b3d795d6e7ed803d3738e
himansu <himansu.behera89@gmail.com>
2021-05-20 10:57:37 +0530

    sample resume website

---
+++ resumeproject/contact/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def contact(request):
+    context={'contact':'active'}
+    return render(request,"contact/contact.html",context)
